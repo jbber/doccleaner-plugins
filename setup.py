d83ac61dcaa750036df1d0a2c19778d0ed21cb9c
jeanbaptisteb <jean-baptiste.bertrand@openedition.org>
2014-07-18 14:39:20 +0200

    rewritten for python 3
    rewritten for python 3, to solve several issues with the win32com
    package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='doccleaner-plugins',
-      version='0.1.0',
+      version='0.2.0',
       description='Plugins to connect the doccleaner module to text processing softwares (MS Word, LibreOffice Writer)',
       url='',
       download_url='',
