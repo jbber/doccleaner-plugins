f618136c1928b762d3164a7151f963c4b646806a
jbber <jean-baptiste.bertrand@openedition.org>
2014-12-03 12:03:26 +0100

    updating macword addin + winword ribbon xml

--- dc_macword/install_dc_macword.py
+++ dc_macword/install_dc_macword.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 01 11:18:57 2014
+
+@author: Bertrand
+"""
+
 #coding: utf-8 -*-
 #A script to generate applescripts calling doccleaner from the MacWord Script menu
 #Not working yet!
@@ -5,6 +12,8 @@
 import os
 import doccleaner
 from string import Template
+import simplejson
+
 #TODO: localization of datadir (or getting it dynamically, but how?):
 #fr = "Données\ utilisateurs\ Microsoft"
 #en = "Microsoft\ Users\ Data"
@@ -15,38 +24,60 @@ from string import Template
 
 datadir = r"Données\ utilisateurs\ Microsoft"
 SCRIPTS_PATH = os.path.join(r"~", r"Documents", datadir, r"Word\ Script\ Menu\ Items")
+def load_json(filename):
+    f = open(filename, "r")
+    data = f.read()
+    f.close()
+    return simplejson.loads(data)
 
-for path, subdirs, files in os.walk(os.path.join(os.path.dirname(doccleaner.__file__), 'docx')):
-     for filename in files:
-        if filename.endswith(".xsl"):
-            #Writing the applescript to the "~/Documents/Données\ utilisateurs\ Microsoft/Word\ Script\ Menu\ Items" folder
-            with open("template.applescript", "r") as f:
-                contents = f.read()
+jsonConf = load_json(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'addin.json'))
+path_dict = {}
+jj = 0
+for button in jsonConf["buttons"]:
 
-            path_dict = {'DOCCLEANER_PATH': '"' + os.path.dirname(doccleaner.__file__) + '"',
-                         'XSL_PATH':        '"' + os.path.join(path,filename) + '"'}
-            
-            #Telling Python that the contents of "template.applescript" are a template:
-            tpl = Template(contents)
-            
-            #passing path_dict to the template
-            contents = tpl.substitute(path_dict)
-            
-            #escaping the quotes
-            contents = contents.replace('"', '\"')
-            
-            #Generating a list of commands for osacompile
-            command_list = (contents.expandtabs()).split("\n")
-            
-            #adding "-e" parameter at the start of each list element. Don't add parameter if line is empty
-            command_list = [' -e "{0}"'.format(command) for command in command_list if command]
-            
-            #appending a -o parameter to the list, containing the path to the script we want to compile
-            processing_name = os.path.splitext(filename)[0] #TODO: giving a friendlier, localized name to this variable
-            command_list.append(' -o '+ os.path.join(SCRIPTS_PATH,  processing_name +".scpt") )
-            
-            #launching the compilation with osacompile 
-            os.system("osacompile " + ''.join(command_list)) #TODO: test...
-             
+    DOCCLEANER_PATH = '"' + os.path.dirname(doccleaner.__file__) + '"'
+
+    #path_dict['DOCCLEANER_PATH'] = DOCCLEANER_PATH
+    XSLNUMBER = len(button["xsl"])
+    for xsl in button["xsl"]:
+
+        
+        with open("template.applescript", "r") as f:
+            contents = f.read()
+        
+        xslid = str(xsl["xslid"])
+                                                       
+        path_dict[xslid] = {'XSL_PATH': str( os.path.join(os.path.dirname(__file__), "docx", xsl["XSLname"]) ), 
+                            'SUBFILE': str( os.path.join(os.path.dirname(__file__), "docx", xsl["subfile"]) ),
+                            'XSLPARAMETER':str( xsl["XSLparameter"] ),
+                            'DOCCLEANER_PATH': DOCCLEANER_PATH,
+                            'XSLNUMBER': XSLNUMBER
+                            }
+    
+    
+    path_dict[xslid]["PROCESSINGS"]= str(path_dict)
+
+    #Telling Python that the contents of "template.applescript" are a template:
+    tpl = Template(contents)
+    
+    #passing path_dict to the template
+    contents = tpl.substitute(path_dict[xslid])
+
+    #escaping the quotes
+    contents = contents.replace('"', '\"')
+                
+    #Generating a list of commands for osacompile
+    command_list = (contents.expandtabs()).split("\n")
+    
+    #adding "-e" parameter at the start of each list element. Don't add parameter if line is empty
+    command_list = [' -e "{0}"'.format(command) for command in command_list if command]
+    
+    #appending a -o parameter to the list, containing the path to the script we want to compile
+    processing_name = os.path.splitext(filename)[0] #TODO: giving a friendlier, localized name to this variable
+    command_list.append(' -o '+ os.path.join(SCRIPTS_PATH,  processing_name +".scpt") )
+
+    #launching the compilation with osacompile 
+    os.system("osacompile " + ''.join(command_list)) #TODO: test...
+         
 
 
--- dc_macword/template.applescript
+++ dc_macword/template.applescript
@@ -19,26 +19,47 @@ tell application "Microsoft Word"
 	-- retrieving path of doccleaner
 	set py to "doccleaner.py "
 	workingDir = $DOCCLEANER_PATH
-	set doccleaner to quoted form of workingDir & py & " -i " & originalDocPath & " -o " & tempFile & " -t " & $XSL_PATH
 	
-	-- launching doccleaner
-	-- TODO: handling the parameters --subfile and --xslparameters
-	do shell script doccleaner
+	set transitionalDoc to originalDocPath
+	--loop here
+	set jj to 1
 	
-	-- opening the tempFile created by doccleaner
-	activate (open tempFile)
+	repeat $XSLNUMBER times
+
+		if (jj > 1) then	
+			set transitionalDoc to tempFile
+			set tempFile to quoted form of tempDir & "~" & jj & originalDocName
+			set tempFile to POSIX file tempFile as alias
+		end if
+		set xslpath to item XSL_PATH of item jj of $PROCESSINGS
+		set subfile to item SUBFILE of item jj of $PROCESSINGS
+		set xslparameter to item XSLPARAMETER of item jj of $PROCESSINGS
+		if $XSLPARAMETER != "0" 
+			set doccleaner to quoted form of workingDir & py & " -i " & transitionalDoc & " -o " & tempFile & " -t " & XSL_PATH & " -s " & SUBFILE & " - p "& XSLPARAMETER
+		else
+			set doccleaner to quoted form of workingDir & py & " -i " & transitionalDoc & " -o " & tempFile & " -t " & XSL_PATH & " -s " & SUBFILE
+		end if
+		-- launching doccleaner
+		do shell script doccleaner
+		
+		-- opening the tempFile created by doccleaner
+		activate (open tempFile)
+		
+		-- passing its content to the newContent variable
+		set newContent to formatted text of text object of document tempFile
+		
+		-- reactivate initial doc
+		activate (open originalDocPath)
+		
+		-- copy content from tempFile to originalDoc, without using the clipboard (it would be bad practice)
+		set formatted text of text object of active document to newContent
+		
+		-- closing the temp file
+		close document tempFile
+		
+		set jj to (jj + 1)
+	end repeat
 	
-	-- passing its content to the newContent variable
-	set newContent to formatted text of text object of document tempFile
-	
-	-- reactivate initial doc
-	activate (open originalDocPath)
-	
-	-- copy content from tempFile to originalDoc, without using the clipboard (it would be bad practice)
-	set formatted text of text object of active document to newContent
-	
-	-- closing the temp file
-	close document tempFile
 end tell
 
 -- removing the tempFile
